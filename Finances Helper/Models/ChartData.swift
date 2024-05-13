97f5e10445df5be3251f78f89188026b3bba365f
Jaipreet Singh <40367262+jaipreetsingh01@users.noreply.github.com>
2024-05-14 03:51:40 +1000

    Replaced Preview Data, added Comments

--- Finances Helper/Models/ChartData.swift
+++ Finances Helper/Models/ChartData.swift
@@ -8,11 +8,12 @@
 import Foundation
 import SwiftUI
 
+// chart variables 
 struct ChartData: Identifiable, Equatable {
     var id: String = UUID().uuidString
     var color : Color
     var slicePercent : CGFloat = 0.0
-    var persentage: Double = 0.0
+    var percentage: Double = 0.0
     var value : Double
     var title: String
     var type: TransactionType = .income
@@ -23,10 +24,12 @@ struct ChartData: Identifiable, Equatable {
     }
 }
 
+
+// this is used for the preview only , udemy kindle netflix psn etc etc 
 extension ChartData {
     static var sample: [ChartData] {
-        [ ChartData(color: .orange, slicePercent: 0.2967032967032967, persentage: 0.2967032967032967, value: 405.0, title: "Udemy"),
-          ChartData(color: .mint, slicePercent: 0.7069597069597069, persentage: 0.41025641025641024, value: 560.0, title: "Kindle"),
-          ChartData(color: .teal, slicePercent: 1.0, persentage: 0.29304029304029305, value: 400.0, title: "Medium")]
+        [ ChartData(color: .red, slicePercent: 0.2967032967032967, percentage: 0.2967032967032967, value: 405.0, title: "Netflix"),
+          ChartData(color: .blue, slicePercent: 0.7069597069597069, percentage: 0.41025641025641024, value: 560.0, title: "PSN"),
+          ChartData(color: .green, slicePercent: 1.0, percentage: 0.29304029304029305, value: 400.0, title: "ActivateUTS")]
     }
 }
