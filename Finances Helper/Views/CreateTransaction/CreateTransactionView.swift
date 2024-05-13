a4af87c0f4d639196dbe243a73a98a2cba017a48
25118314 <164326297+Kjunio791228@users.noreply.github.com>
2024-05-14 02:48:02 +1000

    Update CreateTransactionView.swift

--- Finances Helper/Views/CreateTransaction/CreateTransactionView.swift
+++ Finances Helper/Views/CreateTransaction/CreateTransactionView.swift
@@ -1,37 +1,39 @@
+// CreateTransactionView.swift
+// Finances Helper
 //
-//  CreateTransactionView.swift
-//  Finances Helper
-//
-//  Created by Kendrick  on 10/05/24.
-//
+// Created by Kendrick on 10/05/24.
 
 import SwiftUI
 
+// SwiftUI view for creating a new transaction
 struct CreateTransactionView: View {
-    @Environment(\.dismiss) private var dismiss
-    let type: TransactionType
-    @ObservedObject var rootVM: RootViewModel
-    @StateObject private var viewModel:  CreateTransactionViewModel
+    @Environment(\.dismiss) private var dismiss // Environment variable to dismiss the view
+    let type: TransactionType // Type of transaction (income or expense)
+    @ObservedObject var rootVM: RootViewModel // View model for the root view
+    @StateObject private var viewModel: CreateTransactionViewModel // View model for creating transactions
     
-    init(type: TransactionType, rootVM: RootViewModel){
+    // Initialize the view with the provided transaction type and view model
+    init(type: TransactionType, rootVM: RootViewModel) {
         self.type = type
         self._rootVM = ObservedObject(wrappedValue: rootVM)
         self._viewModel = StateObject(wrappedValue: CreateTransactionViewModel(context: rootVM.coreDataManager.mainContext, transactionType: rootVM.currentTab))
     }
+    
     var body: some View {
         NavigationStack {
             ScrollView(.vertical, showsIndicators: false) {
-                VStack(spacing: 32){
-                    dateButton
-                    amountTextFiled
-                    noteTextField
-                    CategoriesTagsView(createVM: viewModel)
+                VStack(spacing: 32) {
+                    dateButton // Date picker button
+                    amountTextField // Text field for entering the amount
+                    noteTextField // Text field for entering the note
+                    CategoriesTagsView(createVM: viewModel) // View for selecting categories
                 }
                 .padding()
             }
-            .navigationTitle("New "+type.title)
+            .navigationTitle("New " + type.title)
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
+                // Cancel button
                 ToolbarItem(placement: .cancellationAction) {
                     Button {
                         dismiss()
@@ -40,12 +42,12 @@ struct CreateTransactionView: View {
                     }
                 }
                 
+                // Save button
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
-                        if let account = rootVM.activeAccount{
+                        if let account = rootVM.activeAccount {
                             viewModel.create(type: type, date: viewModel.date, forAccount: account, created: rootVM.userService.currentUser)
                         }
-                       
                         dismiss()
                     } label: {
                         Text("Save")
@@ -54,36 +56,31 @@ struct CreateTransactionView: View {
                 }
             }
             .overlay {
-                createCategoryView
+                createCategoryView // Overlay view for creating a new category
             }
         }
         .preferredColorScheme(.light)
     }
 }
 
+// Preview provider for CreateTransactionView
 struct CreateTransactionView_Previews: PreviewProvider {
     static var previews: some View {
         CreateTransactionView(type: .income, rootVM: RootViewModel(context: dev.viewContext))
     }
 }
 
-extension CreateTransactionView{
-    private var dateButton: some View{
-        
+// Extension to CreateTransactionView for private functions
+extension CreateTransactionView {
+    // Date picker button
+    private var dateButton: some View {
         DatePicker(selection: $viewModel.date, displayedComponents: .date) {}
             .labelsHidden()
             .hLeading()
-        
-//        Button {
-//
-//        } label: {
-//            Label(rootVM.selectedDate.toFriedlyDate, systemImage: "calendar")
-//                .font(.headline.bold())
-//                .foregroundColor(.black)
-//        }
     }
     
-    private var amountTextFiled: some View{
+    // Text field for entering the amount
+    private var amountTextField: some View {
         HStack {
             NumberTextField(value: $viewModel.amount, promt: "", label: nil)
             Text(rootVM.activeAccount?.currencyCode ?? "USD")
@@ -91,7 +88,8 @@ extension CreateTransactionView{
         }
     }
     
-    private var noteTextField: some View{
+    // Text field for entering the note
+    private var noteTextField: some View {
         VStack {
             TextField("Note", text: $viewModel.note)
                 .font(.title3.weight(.medium))
@@ -99,9 +97,10 @@ extension CreateTransactionView{
         }
     }
     
-    private var createCategoryView: some View{
-        Group{
-            if let type = viewModel.createCategoryViewType{
+    // Overlay view for creating a new category
+    private var createCategoryView: some View {
+        Group {
+            if let type = viewModel.createCategoryViewType {
                 Color.secondary.opacity(0.3).ignoresSafeArea()
                     .onTapGesture {
                         viewModel.createCategoryViewType = nil
