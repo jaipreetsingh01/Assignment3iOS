dcdccfe95de26b4330b8f455df2317829a5edb9c
25118314 <164326297+Kjunio791228@users.noreply.github.com>
2024-05-13 23:54:27 +1000

    Update CategoryEntity.swift

--- Finances Helper/Service/Model+Extansions/CategoryEntity.swift
+++ Finances Helper/Service/Model+Extansions/CategoryEntity.swift
@@ -1,41 +1,44 @@
-//
 //  CategoryEntity.swift
 //  Finances Helper
 //
 //  Created by Kendrick  on 10/05/24.
 //
 
-
+// Extension for CategoryEntity providing additional functionality
 import Foundation
 import CoreData
 import SwiftUI
 
-extension CategoryEntity{
-    
+// Extension for CategoryEntity
+extension CategoryEntity {
   
-    var wrappedCategoryType: TransactionType{
+    // Computed property to get and set category type
+    var wrappedCategoryType: TransactionType {
         get { TransactionType(rawValue: type ?? "") ?? .expense }
         set { type = newValue.rawValue }
     }
 
+    // Computed property to get and set subcategories
     var wrappedSubcategories: Set<CategoryEntity> {
         get { (subcategories as? Set<CategoryEntity>) ?? [] }
         set { subcategories = newValue as NSSet }
     }
     
-    var wrappedColor: Color{
+    // Computed property to get color as SwiftUI Color
+    var wrappedColor: Color {
         guard let color else { return .blue }
         return Color(hex: color)
     }
     
-    
-    static func request() -> NSFetchRequest<CategoryEntity>{
+    // Static method to create a fetch request for CategoryEntity
+    static func request() -> NSFetchRequest<CategoryEntity> {
         let fetchRequest = NSFetchRequest<CategoryEntity>(entityName: "CategoryEntity")
         fetchRequest.predicate = NSPredicate(format: "isParent == YES")
         fetchRequest.sortDescriptors = [NSSortDescriptor(key: "createAt", ascending: true)]
         return fetchRequest
     }
     
+    // Static method to create a CategoryEntity
     @discardableResult
     static func create(
         context: NSManagedObjectContext,
@@ -44,7 +47,7 @@ extension CategoryEntity{
         subcategories: Set<CategoryEntity>?,
         isParent: Bool,
         type: TransactionType
-    ) -> CategoryEntity{
+    ) -> CategoryEntity {
         let category = CategoryEntity(context: context)
         category.id = UUID().uuidString
         category.createAt = Date.now
@@ -52,12 +55,10 @@ extension CategoryEntity{
         category.title = title
         category.isParent = isParent
         category.wrappedCategoryType = type
-        if let subcategories{
+        if let subcategories {
             category.wrappedSubcategories = subcategories
         }
         
         return category
     }
-
-    
 }
