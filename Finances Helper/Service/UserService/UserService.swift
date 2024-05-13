573c97c8aeb02b8601744826e6d017ffa0df0f23
lansdero <133485221+lansdero@users.noreply.github.com>
2024-05-14 06:25:05 +1000

    Update UserService.swift - Smoothen fetch and update

--- Finances Helper/Service/UserService/UserService.swift
+++ Finances Helper/Service/UserService/UserService.swift
@@ -5,25 +5,27 @@
 //  Created by Lance  on 10/05/24.
 //
 
+
 import Foundation
 import SwiftUI
 import CoreData
 
+// Declare UserService as a final class and an observable object to be used with SwiftUI views
 final class UserService: ObservableObject{
+
+    @AppStorage("currentUserId") private var currentUserId: String = "" // Use of AppStorage to persist the current user's ID across app launches
+    @Published var currentUser: UserEntity? // Use of Published to make currentUser a published property, allowing views to update when it changes
+    let coreDataManager: CoreDataManager // A reference to CoreDataManager which is to handle all core data interactions
     
-    @AppStorage("currentUserId") private var currentUserId: String = ""
-    @Published var currentUser: UserEntity?
-    let coreDataManager: CoreDataManager
-    
-    
+    // Initialize the UserService with a NSManagedObjectContext
     init(context: NSManagedObjectContext){
         self.coreDataManager = CoreDataManager(mainContext: context)
-        fetchUser()
+        fetchUser() // Fetch the current user on initialization
     }
     
-    
+    // Method to fetch the current user from the Core Data
     func fetchUser(){
-        currentUser = coreDataManager.getCurrentUser(id: currentUserId)
-        currentUserId = currentUser?.id ?? ""
+        currentUser = coreDataManager.getCurrentUser(id: currentUserId) // Fetch the user with the currentUserId and update currentUser
+        currentUserId = currentUser?.id ?? "" // Update the currentUserId in case it has changed
     }
 }
